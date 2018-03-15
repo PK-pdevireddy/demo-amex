39963f3805b1714b533d3b5924db6941a900cd12
pdevire1 <praveen.devireddy1@t-mobile.com>
2018-03-15 02:30:56 -0400

    Clean up

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
    id INT NOT NULL,
    firstname VARCHAR(50) NOT NULL,
    lastname VARCHAR(20) NOT NULL,
    designation VARCHAR(20) NOT NULL,
-   salary        INTEGER,
+   salary        INTEGER
 );
